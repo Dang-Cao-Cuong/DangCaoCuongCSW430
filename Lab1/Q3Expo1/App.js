8746772c85e78db13093f8e3e878e2a8c906c4de
Dang-Cao-Cuong <cuong.dangcao.cit22@eiu.edu.vn>
2025-10-18 16:30:38 +0700

    move to Lab1

---
+++ Lab1/Q3Expo1/App.js
@@ -0,0 +1,16 @@
+import React,{ useState } from 'react';
+import {  Text,Button, View } from 'react-native';
+
+export default () =>{
+  const [pressCount, setPressCount] = useState(0);
+  return (
+    <View style={{ alignItems: 'center', marginTop: 20 }}>
+      <Text>You pressed the button {pressCount} times(s)</Text>
+      <Button title="Press me" onPress={() => setPressCount(pressCount + 1)} />
+      <Text> Đặng Cao Cường</Text>
+    </View>
+  );
+};
+
+
+
