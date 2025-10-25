e31e75ae6b7fc98f9e68ee250daf7395042277ed
Dang-Cao-Cuong <cuong.dangcao.cit22@eiu.edu.vn>
2025-10-25 21:05:41 +0700

    add Q4

---
+++ Lab2/Q3IncomeTax/style.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 20,
+    color: 'white',
+    marginBottom: 10,
+  },
+
+  input: {
+    width: 200,
+    color: 'white',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+  },
+  result: {
+    marginTop: 20,
+    color: 'white',
+    fontSize: 16,
+  },
+});
